ab2a9b403e38ead2ea20c18a9a9c207ef11bcbb0
Suhyeon Lee <suhyeon917917@gmail.com>
2019-05-27 10:11:01 +0900

    티백 별로 시간 카운트다운 표시

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -28,20 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.btTea = new System.Windows.Forms.Button();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.cbList = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // btTea
+            // 
+            this.btTea.Location = new System.Drawing.Point(13, 38);
+            this.btTea.Name = "btTea";
+            this.btTea.Size = new System.Drawing.Size(249, 45);
+            this.btTea.TabIndex = 4;
+            this.btTea.Text = "담그기!";
+            this.btTea.UseVisualStyleBackColor = true;
+            this.btTea.Click += new System.EventHandler(this.btTea_Click);
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(13, 89);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.ReadOnly = true;
+            this.txtTime.Size = new System.Drawing.Size(249, 21);
+            this.txtTime.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "2019-05-27 3512 이수현";
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // cbList
+            // 
+            this.cbList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbList.FormattingEnabled = true;
+            this.cbList.Location = new System.Drawing.Point(12, 12);
+            this.cbList.Name = "cbList";
+            this.cbList.Size = new System.Drawing.Size(250, 20);
+            this.cbList.TabIndex = 7;
+            this.cbList.SelectedIndexChanged += new System.EventHandler(this.cbList_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(274, 210);
+            this.ClientSize = new System.Drawing.Size(274, 145);
+            this.Controls.Add(this.cbList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.btTea);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "TeaBagMaker";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Button btTea;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.ComboBox cbList;
     }
 }
 
--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -7,13 +7,92 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using Microsoft.VisualBasic;
+
 namespace TeaBagMaker
 {
     public partial class Form1 : Form
     {
+        int CountOrgNum = 0;
+        string[] TeaList = new string[] { "홍차", "녹차", "루이보스차", "국화차" };
+        string orgStr = "";
+
+        int blackTea;
+        int greenTea;
+        int rooibosTea;
+        int chrysanthemum;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i=0; i<TeaList.Length; i++)
+            {
+                this.cbList.Items.Add(TeaList[i]);
+            }
+            if (cbList.Items.Count > 0)
+            {
+                this.cbList.SelectedIndex = 0;
+            }
+        }
+
+        private void btTea_Click(object sender, EventArgs e)
+        {
+          if(orgStr == "홍차")
+            {
+                MessageBox.Show("홍차를 선택하셨습니다!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.blackTea = 120;
+                this.CountOrgNum = Convert.ToInt32(this.blackTea);
+                this.Timer.Enabled = true;
+            }
+            else if (orgStr == "녹차")
+            {
+                MessageBox.Show("녹차를 선택하셨습니다!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.greenTea = 180;
+                this.CountOrgNum = Convert.ToInt32(this.greenTea);
+                this.Timer.Enabled = true;
+            }
+            else if (orgStr == "루이보스차")
+            {
+                MessageBox.Show("루이보스차를 선택하셨습니다!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.rooibosTea = 300;
+                this.CountOrgNum = Convert.ToInt32(this.rooibosTea);
+                this.Timer.Enabled = true;
+            }
+            else if (orgStr == "국화차")
+            {
+                MessageBox.Show("국화차를 선택하셨습니다!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.chrysanthemum = 120;
+                this.CountOrgNum = Convert.ToInt32(this.chrysanthemum);
+                this.Timer.Enabled = true;
+            }
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (CountOrgNum < 1)
+            {
+                this.Timer.Enabled = false;
+                MessageBox.Show("티백을 건지세요!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtTime.Text = "";
+                this.cbList.Focus();
+            }
+            else
+            {
+                this.CountOrgNum--;
+                this.txtTime.Text = Convert.ToString(this.CountOrgNum);
+            }
+        }
+
+        private void cbList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cbList.SelectedIndex >= 0)
+            {
+                this.orgStr = this.cbList.Text;
+            }
         }
     }
 }
